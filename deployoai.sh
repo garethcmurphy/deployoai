dcd073b23787668c2c9c6ff08f827b20231a2ea0
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-19 12:54:39 +0200

    add multi-site configure

--- deployoai.sh
+++ deployoai.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 git pull
 helm del --purge oai
-helm install oai --name oai --namespace dev
+
+
+if  [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
+    envarray=(ess)
+    INGRESS_NAME="-f ./oai/lund.yaml"
+    BUILD="false"
+elif  [ "$(hostname)" == "k8-lrg-serv-prod.esss.dk" ]; then
+    envarray=(dmscprod)
+    INGRESS_NAME="-f ./oai/dmscprod.yaml"
+    BUILD="false"
+fi
+
+helm install oai --name oai --namespace dev ${INGRESS_NAME}
 # --set image.tag=latest --set image.repository garethcmurphy/oai
