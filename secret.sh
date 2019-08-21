7a12b688d26c071874b5a7017795682bf9a86474
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-21 06:42:08 +0200

    added path to secret

--- secret.sh
+++ secret.sh
@@ -4,6 +4,6 @@ if  [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
     openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout oai.key -out oai.crt -subj "/CN=scicat01.esss.lu.se" -days 3650
     kubectl create secret -ndev tls oaiservice --key oai.key --cert oai.crt
 elif  [ "$(hostname)" == "k8-lrg-serv-prod.esss.dk" ]; then
-    openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout oai.key -out oai.crt  -subj "/CN=scicat.esss.se" -days 3650
+    openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout oai.key -out oai.crt  -subj "/CN=catanieservice.esss.dk/scicat" -days 3650
     kubectl create secret -ndev tls oaiservice --key oai.key --cert oai.crt
 fi
